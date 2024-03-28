c9dcbda401e57dea39c700f092f09bc15bcb2d36
Lilie Lin <lilin@c2r2p6.42vienna.com>
2024-03-28 15:39:37 +0100

    work on launch executable

--- main.c
+++ main.c
@@ -1,18 +1,118 @@
 #include "minishell.h"
 #include "libft/libft.h"
+#include <linux/limits.h>
+#include <readline/history.h>
+#include <stdlib.h>
 
-void	display_prompt()
+char	*build_prompt()
 {
-	char	*user;
 	char	*prompt;
+	char	*user;
+	int	fd_hostname;
+	int	bytes_read;
+	char	hostname_buffer[254];
+	int	hostname_len;
+	char	*path;
+	char	*home;
+	char	*temp;
+
+
 
 	user = getenv("USER");
 	prompt = ft_strjoin(user, "@");
 
-	readline(prompt);
+	prompt = ft_strjoin("fake_", prompt);
+
+	fd_hostname = open("/etc/hostname", O_RDONLY);
+	bytes_read = read(fd_hostname, hostname_buffer, 254);
+	close(fd_hostname);
+	hostname_len = strchr(hostname_buffer, '.') - hostname_buffer;
+	hostname_buffer[hostname_len] = '\0';
+	prompt = ft_strjoin(prompt, hostname_buffer);
+
+	prompt = ft_strjoin(prompt, ":");
+
+	path = getenv("PWD");
+	home = getenv("HOME");
+	temp = ft_strnstr(path, home, ft_strlen(home));
+	if (temp)
+	{
+		ft_strlcpy(path, path + ft_strlen(home), ft_strlen(path) - ft_strlen(home) + 1);
+		prompt = ft_strjoin(prompt, "~");
+	}
+	prompt = ft_strjoin(prompt, path);
+	prompt = ft_strjoin(prompt, "$ ");
+
+	return (prompt);
 }
 
-int	main(void)
+void	execute_command(char *input, char **envp)
 {
-	display_prompt();
+	char	*path;
+	char	**path_array;
+	char	**input_array;
+	int	i;
+	char	*command_path;
+	int	fd;
+
+	path = getenv("PATH");
+	path_array = ft_split(path, ':');
+	input_array = ft_split(input, ' ');
+	i = 0;
+	while (path_array[i])
+	{
+
+		command_path = ft_strjoin(path_array[i], "/");
+		command_path = ft_strjoin(command_path, input_array[0]);
+		fd = access(command_path, F_OK & X_OK);
+		if(fd == 0)
+		{
+			printf("%s\n", input_array[0]);
+			printf("%s\n", command_path);
+			execve(command_path, input_array, envp);
+		}
+		i++;
+	}
+	exit(EXIT_FAILURE);
+
+}
+
+void	display_prompt(int sigint)
+{
+	char	*prompt;
+	char	*input;
+
+	(void)sigint;
+	prompt = build_prompt();
+	input = readline(prompt);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*prompt;
+	char	*input;
+	int	pid;
+	int	status;
+
+	(void)argc;
+	(void)argv;
+
+
+
+	prompt = build_prompt();
+
+	input = readline(prompt);
+	while (input)
+	{
+
+		add_history(input);
+		pid = fork();
+		if (pid == 0)
+			execute_command(input, envp);
+		wait(&status);
+		input = readline(prompt);
+	}
+
+
+	signal(SIGINT, display_prompt);
 }
--- minishell.h
+++ minishell.h
@@ -1,8 +1,13 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <unistd.h>
+# include <fcntl.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <limits.h>
+# include <sys/wait.h>
+# include <signal.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "libft/libft.h"
