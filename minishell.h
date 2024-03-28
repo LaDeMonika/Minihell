a7999965eb93d89de3eb38d041186878b3d6a578
Monika <128793184+LaDeMonika@users.noreply.github.com>
2024-03-28 14:17:02 +0000

    add some incs and struct

--- minishell.h
+++ minishell.h
@@ -1,10 +1,22 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+//**INCLUDES
+# include "libft/libft.h"
 # include <stdlib.h>
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include "libft/libft.h"
+# include <signal.h>
+# include <fcntl.h>
+
+//**STRUCT
+typedef struct s_minishell
+{
+    //user input
+    //tokens
+    //env
+    //and so on what we need
+}               t_minishell
 
 #endif
