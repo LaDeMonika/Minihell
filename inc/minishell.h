102f6f089d5223104b119b8154a7603a242c3dc6
Monika Simic <msimic@c2r1p1.42vienna.com>
2024-04-08 11:23:18 +0200

    execute_command func parted

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: msimic <msimic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 15:21:51 by msimic            #+#    #+#             */
-/*   Updated: 2024/04/04 14:52:40 by msimic           ###   ########.fr       */
+/*   Updated: 2024/04/08 11:21:45 by msimic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,17 +31,19 @@
 //***************************STRUCT
 typedef struct s_minishell
 {
-    char *usr_input; // User input
-    char *prompt; // Prompt
-    char *home_dir; // Home directory
-    char *current_dir; // Current working directory
-    char *command_path; // Path to the command
-    char **envp; // Environment variables
-    char **input_array; // Array of input arguments
-    char **command_history; // Array of command history
-    int history_index; // Index of the command history
-    int last_exit_status; // Exit status of the last command
-    int fd; // File descriptor
+    char    *usr_input; // User input
+    char    *prompt; // Prompt
+    int     len_prompt; // Length of the prompt
+    char    **path_array; // Array of paths
+    char    *home_dir; // Home directory
+    char    *current_dir; // Current working directory
+    char    *command_path; // Path to the command
+    char    **envp; // Environment variables
+    char    **input_array; // Array of input arguments
+    char    **command_history; // Array of command history
+    int     history_index; // Index of the command history
+    int     last_exit_status; // Exit status of the last command
+    int     fd; // File descriptor
 }               t_minishell;
 
 //***************************ENUM
--- src/main/minishell.c
+++ src/main/minishell.c
@@ -6,17 +6,16 @@
 /*   By: msimic <msimic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 15:21:45 by msimic            #+#    #+#             */
-/*   Updated: 2024/04/08 10:44:21 by msimic           ###   ########.fr       */
+/*   Updated: 2024/04/08 11:19:27 by msimic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-void execute_command(char *input, t_minishell *shell)
+void find_path(t_minishell *shell)
 {
 	int i;
 
-	shell->input_array = ft_split(input, ' ');
 	i = 0;
 	while (shell->envp[i])
 	{
@@ -26,6 +25,57 @@ void execute_command(char *input, t_minishell *shell)
 	}
 	if (!shell->envp[i])
 		return(ft_error_msg(ERR_PATH_NOT_FOUND));
+	shell->path_array = ft_split(shell->envp[i] + 5, ':');
+}
+
+void	execute_command_path(t_minishell *shell, int i)
+{
+	shell->command_path = ft_strjoin(shell->path_array[i], "/");
+	shell->command_path = ft_strjoin(shell->command_path, shell->input_array[0]);
+	shell->fd = access(shell->command_path, F_OK & X_OK);
+	if (shell->fd == 0)
+	{
+		pid_t pid = fork();
+
+		if (pid < 0)
+		{
+		    // Fork failed
+		    perror("fork");
+		}
+		else if (pid == 0)
+		{
+		    // Child process
+		    execve(shell->command_path, shell->input_array, shell->envp);
+		    // If execve returns, it must have failed
+		    perror("execve");
+		    exit(EXIT_FAILURE);
+		}
+		else
+		{
+		    // parent process
+		    int status;
+		    waitpid(pid, &status, 0);
+		}
+	}
+
+}
+
+void execute_command(char *input, t_minishell *shell)
+{
+	int i;
+
+	shell->input_array = ft_split(input, ' ');
+	find_path(shell);
+	i = 0;
+	while (shell->path_array[i])
+	{
+		execute_command_path(shell, i);
+		i++;
+	}
+}
+/* 
+void execute_command(char *input, t_minishell *shell)
+{
 	char **path_array = ft_split(shell->envp[i] + 5, ':');
 	i = 0;
 	while (path_array[i])
@@ -59,7 +109,7 @@ void execute_command(char *input, t_minishell *shell)
 		}
 		i++;
 	}
-}
+} */
 
 int main(int argc, char **argv, char **envp)
 {
