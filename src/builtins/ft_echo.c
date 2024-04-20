f3aa5bcbd66675cda2c7e21cc5efffe4a9269143
Monika Simic <msimic@c2r1p1.42vienna.com>
2024-04-20 12:52:01 +0200

    fix: for empty args in ft_echo main

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -6,13 +6,13 @@
 /*   By: msimic <msimic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 15:09:56 by msimic            #+#    #+#             */
-/*   Updated: 2024/04/20 12:16:47 by msimic           ###   ########.fr       */
+/*   Updated: 2024/04/20 12:50:52 by msimic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-/* static int ft_strcmp(char *s1, char *s2)
+static int ft_strcmp(char *s1, char *s2)
 {
     int i;
 
@@ -20,12 +20,10 @@
     while (s1[i] && s2[i])
     {
         if (s1[i] != s2[i])
-            return (0);
+            return (1);
         i++;
     }
-    if (s1[i] != s2[i])
-        return (0);
-    return (1);
+    return (s1[i] - s2[i]);
 }
 
 static void	ft_putchar_fd(char c, int fd)
@@ -42,7 +40,7 @@ static void	ft_putstr_fd(char *s, int fd)
 		ft_putchar_fd(*s, fd);
 		s++;
 	}
-} */
+}
 
 /*
 ft_echo is a function that will print the arguments to the standard output.
@@ -63,13 +61,16 @@ int ft_echo(t_minishell *shell)
     return (1);
 }
 
-/* int ft_is_builtin(t_minishell *shell)
+int ft_is_builtin(t_minishell *shell)
 {
     int status;
 
     status = 1; // 1 = builtin, 0 = not builtin
-    if (ft_strcmp(shell->input_array[0], "echo") == 5) // 5 = length of "echo"
+    if (ft_strcmp(shell->input_array[1], "echo") == 0)
+    {
+        printf("Smth\n");
         status = ft_echo(shell);
+    }
     if (status == 0)
         shell->exit_status = 0;
     return (status);
@@ -79,7 +80,13 @@ int main(int ac, char **av)
 {
     t_minishell shell;
 
+    if (ac < 2)
+    {
+        printf("Error: Not enough arguments\n");
+        return (0);
+    }
     shell.input_array = av;
+    printf("Input array: %s\n", shell.input_array[1]);
     ft_is_builtin(&shell);
     return (0);
-} */
+}
