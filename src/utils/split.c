4e3e4677cdd495ff03a376d942f4e5da7af2cac2
Lilie Lin <lilin@c4r7p6.42vienna.com>
2024-05-06 21:39:43 +0200

    fix: quote handling

--- src/utils/split.c
+++ src/utils/split.c
@@ -5,46 +5,7 @@
 #include <stdlib.h>
 
 
-
-
-/* static char	find_quote(char *s, int *start, int *end, int *markers)
-{
-	int		i;
-	char	quote_marker;
-	int		len;
-
-	*start = -1;
-	*end = -1;
-	len = ft_strlen(s);
-	*markers = 0;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == '"' || s[i] == '\'')
-		{
-			quote_marker = s[i];
-			*start = i;
-			*markers += 1;
-			while (s[len - 1])
-			{
-				if (s[len - 1] == quote_marker && len - 1 != *start)
-				{
-					if (*markers == 1)
-						*end = len - 1;
-					*markers += 1;
-				}
-				len--;
-			}
-			return (quote_marker);
-		}
-		i++;
-	}
-	return ('\0');
-}
- */
-
-
-/*s is guaranteed to have an even amount of outer quotes
+/*
 if sep is pipe start word count at one and at one for every pipe
 if sep is space, then start word count at zero and only add for space before other character or null-terminator*/
 static int	count_words(char *s, char sep)
@@ -67,8 +28,8 @@ static int	count_words(char *s, char sep)
 			while (s[i] && s[i] != quote_type)
 				i++;
 		}
-		else if ((sep == '|' && s[i] == sep)
-		|| (sep == ' ' && s[i] != sep && (!s[i + 1] || s[i + 1] != sep)))
+		if ((sep == '|' && s[i] == sep)
+		|| (sep == ' ' && s[i] != sep && (!s[i + 1] || s[i + 1] == sep)))
 			words++;
 		i++;
 	}
@@ -77,20 +38,8 @@ static int	count_words(char *s, char sep)
 /*if sep is | and word is empty than substitute empty word with |*/
 static char	*put_word(char *s, int start, int len, char sep)
 {
-	/* int		i; */
 	char	*word;
 
-	/* i = 0;
-	word = NULL;
-	word = malloc((len + 1) * sizeof(char));
-	if (!word)
-		return (NULL);
-	word[len] = '\0';
-	while (i < len)
-	{
-		word[i] = s[start + i];
-		i++;
-	} */
 	word = ft_substr(s, start, len);
 	word = ft_strtrim(word, " ");
 	if (sep == '|' && (!word || !(*word)))
@@ -115,7 +64,9 @@ static void	free_array(char **array, int word)
 	free(array);
 	array = NULL;
 }
-
+/*
+put word until separator, and skip quotes
+if sep is | and a word is missing, then fill the word with | */
 static int	put_words(char *s, char sep, char **array, int words)
 {
 	int	i;
@@ -135,11 +86,11 @@ static int	put_words(char *s, char sep, char **array, int words)
 			while (s[i] && s[i] != outer_quote)
 				i++;
 		}
-		else if ((sep == '|' && (s[i] == sep || !s[i + 1]))
-		|| (sep == ' ' && s[i] != sep && (!s[i + 1] || s[i + 1] != sep)))
+		if (!s[i]
+		|| (sep == '|' && (s[i] == sep || !s[i + 1]))
+		|| (sep == ' ' && s[i] != sep && (!s[i + 1] || s[i + 1] == sep)))
 		{
-			printf("start: %d i: %d len: %d\n", start, i, i - start);
-			array[word] = put_word(s, start, i - start, sep);
+			array[word] = put_word(s, start, i - start + 1, sep);
 			if (!array[word])
 				return (free_array(array, word), -1);
 			word++;
@@ -147,7 +98,6 @@ static int	put_words(char *s, char sep, char **array, int words)
 		}
 		i++;
 	}
-	printf("word: %d words: %d\n", word, words);
 	if (word < words && sep == '|')
 	{
 		array[word] = malloc(sizeof(char) * 2);
@@ -156,80 +106,15 @@ static int	put_words(char *s, char sep, char **array, int words)
 	return (0);
 }
 
-
-/*
-char *check_env_variables(t_minishell *shell, char *s)
-{
-	int	i;
-	int	start;
-	char	*env_key;
-	char	*env_value;
-	char	*new_s;
-
-	i = 0;
-	start = 0;
-	new_s = NULL;
-	while (s[i])
-	{
-		if (s[i] == '$')
-		{
-			new_s = append_substring(s, start, i, new_s);
-			start = i + 1;
-			while (s[i] && s[i] != ' ' && s[i] != '"')
-					i++;
-			if (s[start] == '?')
-				env_value = ft_itoa(shell->last_exit_status);
-			else
-			{
-				env_key = ft_substr(s, start, i - start);
-				env_value = getenv(env_key);
-			}
-			if (env_value)
-				new_s = ft_strjoin(new_s, env_value);
-			else
-			{
-				new_s = ft_strjoin(new_s, "$");
-				new_s = ft_strjoin(new_s, env_key);
-			}
-
-			start = i;
-
-		}
-		i++;
-	}
-	if (i != start)
-		new_s = append_substring(s, start, i, new_s);
-	return (new_s);
-} */
-/* static char	*add_heredoc(char *old_s, char quote_marker)
-{
-	(void)old_s;
-	(void)quote_marker;
-	return (old_s);
-}
- */
 char	**split_skip_quotes(t_minishell *shell, char *s, char c)
 {
 	int		words;
 	char	**array;
-	/* char	quote_marker;
-	int		start_quote;
-	int		end_quote;
-	int		markers; */
 
 	(void)shell;
 	if (!s || !s[0])
 		return (NULL);
-/* 	quote_marker = find_quote(s, &start_quote, &end_quote, &markers);
-	if (c == '|')
-	{
-		if (!quote_marker || quote_marker == '"')
-			s = check_env_variables(shell, s);
-		if (quote_marker != '\0' && markers % 2 != 0)
-			s = add_heredoc(s, quote_marker);
-	} */
 	words = count_words(s, c);
-	printf("amount of words: %d\n", words);
 	array = NULL;
 	array = malloc((words + 1) * sizeof(char *));
 	if (!array)
@@ -237,12 +122,9 @@ char	**split_skip_quotes(t_minishell *shell, char *s, char c)
 	array[words] = NULL;
 	if (put_words(s, c, array, words) == -1)
 		return (NULL);
-
 	int i = 0;
 	while (array[i])
 	{
-		printf("index: %d word: %s\n", i, array[i]);
-		//free(array[i]);
 		i++;
 	}
 	return (array);
