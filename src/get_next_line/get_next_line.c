a31490f607c1bc1e4c5c1a0b38c2ed7a908c3f36
Lilie Lin <lilin@c2r5p12.42vienna.com>
2024-05-27 13:38:18 +0200

    chore: add get next line

---
+++ src/get_next_line/get_next_line.c
@@ -0,0 +1,183 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lilin <lilin@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 18:30:47 by lilin             #+#    #+#             */
+/*   Updated: 2024/05/18 15:03:21 by lilin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_strchr(char *storage, char c)
+{
+	while (storage && *storage)
+	{
+		if (*storage == c)
+			return (storage);
+		storage++;
+	}
+	return (NULL);
+}
+
+static int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str && str[len])
+		len++;
+	return (len);
+}
+
+static char	*free_and_reset(char **ptr)
+{
+	if (*ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+	}
+	return (NULL);
+}
+
+
+static char	*join_strings(char *storage, char *buffer)
+{
+	char	*joined;
+	int		i;
+
+	joined = malloc(sizeof(char) * (ft_strlen(storage) + ft_strlen(buffer)
+				+ 1));
+	if (!joined)
+		return (free_and_reset(&storage));
+	i = 0;
+	while (storage && storage[i])
+	{
+		joined[i] = storage[i];
+		i++;
+	}
+	free_and_reset(&storage);
+	while (buffer && *buffer)
+	{
+		joined[i] = *(buffer++);
+		i++;
+	}
+	joined[i] = '\0';
+	return (joined);
+}
+
+static char	*extract_line(char *storage, int newline_index)
+{
+	char	*line;
+	int		i;
+
+	line = malloc(sizeof(char) * (newline_index + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (i < newline_index)
+	{
+		line[i] = storage[i];
+		i++;
+	}
+	line[newline_index] = '\n';
+	line[newline_index + 1] = '\0';
+	return (line);
+}
+
+char	*update_storage(char *storage, int newline_index, char **line)
+{
+	char	*updated;
+	int		i;
+
+	if (ft_strlen(storage) - newline_index <= 1)
+		return (free_and_reset(&storage));
+	updated = malloc(sizeof(char) * (ft_strlen(storage) - newline_index));
+	if (!updated)
+		return (free_and_reset(&storage), free_and_reset(line));
+	i = 0;
+	while (storage[i + newline_index + 1])
+	{
+		updated[i] = storage[i + newline_index + 1];
+		i++;
+	}
+	updated[i] = '\0';
+	free_and_reset(&storage);
+	return (updated);
+}
+
+char	*read_and_store(char **storage, int fd)
+{
+	char	*buffer;
+	int		bytes_read;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (free_and_reset(storage));
+	while (!ft_strchr(*storage, '\n'))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read > 0)
+		{
+			buffer[bytes_read] = '\0';
+			*storage = join_strings(*storage, buffer);
+			if (!(*storage))
+				return (free_and_reset(&buffer));
+		}
+		else if (bytes_read < 0)
+			return (free_and_reset(storage), free_and_reset(&buffer));
+		else
+			break ;
+	}
+	free_and_reset(&buffer);
+	return (ft_strchr(*storage, '\n'));
+}
+
+char	*get_next_line(int fd)
+{
+	char		*newline_found;
+	static char	*storage;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	newline_found = read_and_store(&storage, fd);
+	if (newline_found)
+	{
+		line = extract_line(storage, newline_found - storage);
+		if (!line)
+			return (free_and_reset(&storage));
+		storage = update_storage(storage, newline_found - storage, &line);
+	}
+	else if (storage && *storage)
+	{
+		line = storage;
+		storage = NULL;
+	}
+	else
+		line = NULL;
+	return (line);
+}
+
+#include <stdio.h>
+/*
+int	main(void)
+{
+	int     fd;
+	char    *line;
+
+	fd = open("test3.txt", O_RDONLY);
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+}
+
+ */
---
+++ src/get_next_line/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lilin <lilin@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/07 16:54:28 by lilin             #+#    #+#             */
+/*   Updated: 2024/05/18 14:58:44 by lilin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
