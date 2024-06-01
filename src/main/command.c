555ee53340d7832568ebbe974d670715396f9192
Monika Simic <msimic@c2r5p14.42vienna.com>
2024-06-01 16:04:43 +0200

    command.c -> remove_metaquotes devided. Tester: 0 fails

--- src/main/command.c
+++ src/main/command.c
@@ -1,7 +1,4 @@
 #include "../../inc/minishell.h"
-#include <stdbool.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 int	count_literal_chars(char *str, char metaquote)
 {
@@ -28,43 +25,55 @@ int	count_literal_chars(char *str, char metaquote)
 	return (len);
 }
 
-char	*remove_metaquotes(t_minishell *shell, char *str)
+char *initialize(t_minishell *shell, char *str, char *metaquote, int *new_len, int *i, int *j)
 {
-	char	*new_str;
-	int		new_len;
-	int		i;
-	int		j;
-	char	metaquote;
+    *metaquote = '\0';
+    *new_len = count_literal_chars(str, *metaquote);
+    char *new_str = try_malloc(shell, sizeof(char) * (*new_len + 1));
+    *i = 0;
+    *j = 0;
+    return new_str;
+}
 
-	metaquote = '\0';
-	new_len = count_literal_chars(str, metaquote);
-	new_str = try_malloc(shell, sizeof(char) * (new_len + 1));
-	i = 0;
-	j = 0;
-	while (str && str[i])
-	{
-		if (str[i] == '"' || str[i] == '\'')
-		{
-			if (!(metaquote))
-				metaquote = str[i];
-			else if (metaquote == str[i])
-				metaquote = '\0';
-			else if (str[i] != metaquote)
-			{
-				new_str[j] = str[i];
-				j++;
-			}
-		}
-		else
-		{
-			new_str[j] = str[i];
-			j++;
-		}
-		i++;
-	}
-	new_str[j] = '\0';
-	free_and_reset_ptr((void **)&str);
-	return (new_str);
+void process_string(char *str, char *new_str, char *metaquote, int *i, int *j)
+{
+    while (str && str[*i])
+    {
+        if (str[*i] == '"' || str[*i] == '\'')
+        {
+            if (!(*metaquote))
+                *metaquote = str[*i];
+            else if (*metaquote == str[*i])
+                *metaquote = '\0';
+            else if (str[*i] != *metaquote)
+            {
+                new_str[*j] = str[*i];
+                (*j)++;
+            }
+        }
+        else
+        {
+            new_str[*j] = str[*i];
+            (*j)++;
+        }
+        (*i)++;
+    }
+}
+
+char *finalize(char *new_str, char *str, int j)
+{
+    new_str[j] = '\0';
+    free_and_reset_ptr((void **)&str);
+    return new_str;
+}
+
+char *remove_metaquotes(t_minishell *shell, char *str)
+{
+    char metaquote;
+    int new_len, i, j;
+    char *new_str = initialize(shell, str, &metaquote, &new_len, &i, &j);
+    process_string(str, new_str, &metaquote, &i, &j);
+    return finalize(new_str, str, j);
 }
 
 char	*find_command(t_minishell *shell, char **input_array)
