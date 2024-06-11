74fc8985c290674e730ef8651c21888c3b924b5b
Monika Simic <msimic@c4r6p5.42vienna.com>
2024-06-11 12:40:34 +0200

    refactor:

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,27 +6,23 @@
 /*   By: msimic <msimic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 13:21:02 by msimic            #+#    #+#             */
-/*   Updated: 2024/05/31 16:50:50 by msimic           ###   ########.fr       */
+/*   Updated: 2024/06/11 12:38:53 by msimic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-int ft_cd(t_minishell *shell, char **command_array)
+int handle_to_many_args(char **current_pwd)
 {
-    char    *new_pwd;
-    char *current_pwd;
+    free_and_reset_ptr((void **)current_pwd);
+    errno = U_TOO_MANY_ARGUMENTS;
+    return (1);
+}
 
-    current_pwd = getcwd(NULL, 0);
-    if (current_pwd == NULL)
-        error_free_all(shell, ERR_GETCWD, NULL, NULL);
-    if (command_array[1] && command_array[2])
-    {
-        free_and_reset_ptr((void **)&current_pwd);
+char *get_new_pwd(t_minishell *shell, char **command_array)
+{
+    char *new_pwd;
 
-        errno = U_TOO_MANY_ARGUMENTS;
-        return (1);
-    }
     if (!command_array[1] || ft_strcmp(command_array[1], "--") == 0 || ft_strcmp(command_array[1], "~") == 0)
         new_pwd = ft_getenv(shell, "HOME");
     else if (command_array[1][0] == '~')
@@ -40,6 +36,11 @@ int ft_cd(t_minishell *shell, char **command_array)
         new_pwd = ft_strdup(shell, "..");
     else
         new_pwd = ft_strdup(shell, command_array[1]);
+    return (new_pwd);
+}
+
+int change_directory(t_minishell *shell, char *new_pwd, char *current_pwd)
+{
     if (chdir(new_pwd) == -1)
     {
         free_and_reset_ptr((void **)&new_pwd);
@@ -52,5 +53,20 @@ int ft_cd(t_minishell *shell, char **command_array)
     current_pwd = getcwd(NULL, 0);
     update_value(shell, ft_strdup(shell, "PWD"), current_pwd, 0);
     free_and_reset_ptr((void **)&current_pwd);
+    return (0);
+}
+
+int ft_cd(t_minishell *shell, char **command_array)
+{
+    char    *new_pwd;
+    char *current_pwd;
+
+    current_pwd = getcwd(NULL, 0);
+    if (current_pwd == NULL)
+        error_free_all(shell, ERR_GETCWD, NULL, NULL);
+    if (command_array[1] && command_array[2])
+        return (handle_to_many_args(&current_pwd));
+    new_pwd = get_new_pwd(shell, command_array);
+    return (change_directory(shell, new_pwd, current_pwd));
     return (0);
 }
